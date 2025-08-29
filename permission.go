0a61eb1e48bbf53ca7a925e98f51bf2888462689
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2025-08-29 13:44:46 +0000

    fix permission

---
+++ permission.go
@@ -0,0 +1,12 @@
+package common
+
+import "fmt"
+
+type Permission struct {
+	Resource string `json:"resource"`
+	Action   string `json:"action"`
+}
+
+func (p *Permission) String() string {
+	return fmt.Sprintf("%s:%s", p.Resource, p.Action)
+}
